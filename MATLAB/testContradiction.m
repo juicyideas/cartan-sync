4110fbd3c88274cdabbcad424f27529f10deea00
Jesus Briales <jesusbriales@gmail.com>
2017-09-18 11:01:16 +0200

    Add some comments and save contradiction case

--- MATLAB/testContradiction.m
+++ MATLAB/testContradiction.m
@@ -1,31 +1,42 @@
 function testContradiction
 
-n = 150;
-d = 3;
+% n = 150;
+% d = 3;
+d = 2;  % dimension of block
+nb = 3; % number of blocks
 % nze = 0;
-nb = n/d;
 
+% Generate a random (valid) condensed matrix
 m = rnd.cov(nb);
-m = abs(m);
-assert(all(eig(m)>0),'m is not PSD')
+m = abs(m); % force all values are positive (TODO: drop this?)
+assert(all(eig(m)>0),'m is not PSD') % make sure it's PSD
 
+% Create the worst case condensation (negative signs)
+mneg = -m;
+mneg = mneg + 2*diag(diag(m));
+
+% Generate a random block-matrix so that its condensation yields m
 c_M = cell(nb,nb);
 c_ev = cell(nb,nb);
 for i=1:nb
-  % Diagonal element
+  % Diagonal block element
+  % It's built so that its abs(min(eig)) is equal to m_ii
 %   T = rnd.cov(d,1);
   mag = 10^(3*rand);
   T = mag*rand(d);
   [U,S,~] = svd(T);
-  S = diag( diag(S) + m(i,i) ); % make sv_min = m_ii
+  % Displace eigenvalues so that min is m_ii
+  S = diag( diag(S) - min(diag(S)) + m(i,i) ); % make sv_min = m_ii
   c_M{i,i} = U*S*U';
   
   for j=i+1:nb
     % Off-diagonal elements
+    % It's built so that its abs(max(eig)) is equal to m_ij
 %     T = rand(d);
     mag = 10^(3*rand);
     T = mag*rand(d);
     [U,~,V] = svd(T);
+    % Set eigenvalues between 0 and m_ij so that max is m_ij
     S = diag( linspace(0,m(i,j),d) ); % make sv_max = m_ij
     c_M{i,j} = U*S*V';
     c_M{j,i} = c_M{i,j}';
@@ -34,9 +45,10 @@ end
 M = cell2mat(c_M);
 M = symmetrize(M);
 
-% Check if PSD
-ev = eig(M)'
+% Check if the complete block-matrix is PSD
+ev_M = eig(M)'
 
+% Collect block-wise eigenvalues (just for further analysis)
 for i=1:nb
   for j=i:nb
     c_ev{i,j} = sort( eig(c_M{i,j}) );
@@ -44,16 +56,25 @@ for i=1:nb
   end
 end
 
-% Compute m from M again
+% Compute m from M again (to make sure our construction is fine)
 mSV = testBDD(M,d*ones(1,nb));
 assert(norm(m-mSV,'fro')<1e-10,'m \neq mSV')
+ev_m = eig(m)'
 
-if any(ev<0)
+% Check eigenvalues for neg-condensed matrix
+ev_mneg = eig(mneg)'
+
+if any(ev_M<0)
   warning('Contradiction')
   keyboard
+  % Save contradiction for further analysis
+  save('contradiction.mat','M','d','nb')
 end
 
 return
+
+% Deprecated code below (too random)
+% Besides, 2x2 block-matrices always fulfill our conjecture
 A = rnd.cov(n,2)
 
 
